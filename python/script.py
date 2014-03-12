7a2d6f27cf2cda325cb3c94ef215c60f4a134a33
xukai <xukai19871105@126.com>
2014-03-12 22:04:46 +0800

    增加注释

--- python/script.py
+++ python/script.py
@@ -1,20 +1,22 @@
+# -*- coding: utf-8 -*-
 import webiopi
 
 GPIO = webiopi.GPIO
 
-LIGHT = 17 # GPIO pin using BCM numbering
+# GPIO 17 BCM编码方式
+LIGHT = 17 
 
-# setup function is automatically called at WebIOPi startup
+# 初始化程序，webiopi初始化时自动调用
 def setup():
-    # set the GPIO used by the light to output
+    # 设置GPIO 17为输出状态
     GPIO.setFunction(LIGHT, GPIO.OUT)
 
 # loop function is repeatedly called by WebIOPi 
 def loop():
 
-    # gives CPU some time before looping again
+    # 给CPU运行时间
     webiopi.sleep(1)
 
-# destroy function is called at WebIOPi shutdown
+# 回收程序，webiopi停止运行时调用，非常实用
 def destroy():
     GPIO.digitalWrite(LIGHT, GPIO.LOW)
